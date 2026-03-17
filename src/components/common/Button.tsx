2d44e06d2ec7555e41e9e63e0dc001fd3648ee0e
jana-ezzat <janaezzat362@gmail.com>
2026-03-17 02:18:55 +0200

    Added common components

---
+++ src/components/common/Button.tsx
@@ -0,0 +1,23 @@
+interface ButtonProps {
+  text: string;
+  width?: string;
+  onClick?: () => void;
+  type?: "button" | "submit" | "reset";
+}
+
+export default function Button({
+  text,
+  width = "w-full",
+  onClick,
+  type = "button",
+}: ButtonProps) {
+  return (
+    <button
+      type={type}
+      onClick={onClick}
+      style={{ boxShadow: "0 10px 30px 0 rgba(255, 77, 77, 0.2)" }}
+      className={`${width} bg-(--main-color) text-white font-bold py-3 rounded-md cursor-pointer mt-4 hover:opacity-90 transition`}>
+      {text}
+    </button>
+  );
+}
